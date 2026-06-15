59bb52730f3818c7d1117581727a75408ceb9506
whdwananto <whdwananto@gmail.com>
2026-06-15 23:29:30 +0700

    Commit 2

--- js/renderSkills.js
+++ js/renderSkills.js
@@ -8,7 +8,7 @@ export function renderSkills() {
     container.innerHTML = skills.map(skills => `
         <div class="col-lg-3 col-md-4 col-6">
             <div class="card skill-card border-0 h-100">
-                <div class="card-body text-center text-white">
+                <div class="card-body text-center">
                     ${skills}
                 </div>
             </div>
@@ -24,7 +24,7 @@ export function renderTools() {
     container.innerHTML = tools.map(tools => `
         <div class="col-lg-3 col-md-4 col-6">
             <div class="card skill-card border-0 h-100">
-                <div class="card-body text-center text-white">
+                <div class="card-body text-center">
                     ${tools}
                 </div>
             </div>
